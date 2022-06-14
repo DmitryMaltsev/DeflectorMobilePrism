e58037f0f12c25cc9ce4f5b3aa01045ff78890a0
Dmitry <Matltsev_Dmitry91@mail.ru>
2022-06-14 18:24:59 +0300

    123

--- DeflectorMobilePrism/ViewModels/MainPageViewModel.cs
+++ DeflectorMobilePrism/ViewModels/MainPageViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Android.Bluetooth;
 
+using DeflectoreMobilePrism.Core;
+
 using Plugin.BluetoothClassic.Abstractions;
 
 using Prism.Commands;
@@ -21,31 +23,35 @@ namespace DeflectorMobilePrism.ViewModels
 
 
         public new INavigationService NavigationService { get; }
-        private BluetoothDeviceModel _selectedBoundedDevice;
-        public BluetoothDeviceModel SelectedBoundedDevice
+        #region Cопряженные устройства
+        private BluetoothDeviceModel _selectedBondedDevice;
+        public BluetoothDeviceModel SelectedBondedDevice
         {
-            get { return _selectedBoundedDevice; }
+            get { return _selectedBondedDevice; }
             set
             {
-                SetProperty(ref _selectedBoundedDevice, value);
-                if (_selectedBoundedDevice != null)
+                SetProperty(ref _selectedBondedDevice, value);
+                if (_selectedBondedDevice != null)
                 {
                     ExecuteDeviceSelectedCommand();
                 }
             }
         }
-        private IEnumerable<BluetoothDeviceModel> _available_Bounded_Devices;
-        public IEnumerable<BluetoothDeviceModel> Available_Bounded_Devices
+        private IEnumerable<BluetoothDeviceModel> _availableBondedDevices;
+        public IEnumerable<BluetoothDeviceModel> AvailableBondedDevices
         {
-            get { return _available_Bounded_Devices; }
-            set { SetProperty(ref _available_Bounded_Devices, value); }
+            get { return _availableBondedDevices; }
+            set { SetProperty(ref _availableBondedDevices, value); }
         }
 
-        private ObservableCollection<BluetoothDevice> _available_devices;
-        public ObservableCollection<BluetoothDevice> Available_Devices
+        #endregion
+
+        #region Найденные устройства
+        private ObservableCollection<BluetoothDevice> _availabledevices;
+        public ObservableCollection<BluetoothDevice> AvailableDevices
         {
-            get { return _available_devices; }
-            set { SetProperty(ref _available_devices, value); }
+            get { return _availabledevices; }
+            set { SetProperty(ref _availabledevices, value); }
         }
 
         private BluetoothDevice _selectedDevice;
@@ -57,34 +63,89 @@ namespace DeflectorMobilePrism.ViewModels
                 SetProperty(ref _selectedDevice, value);
                 if (_selectedDevice != null)
                 {
-                  //  ExecuteConnectDeviceCommand();
+                    //  ExecuteConnectDeviceCommand();
                 }
             }
         }
+        #endregion
 
+        private DelegateCommand _scanDevicesCommand;
+        public DelegateCommand ScanDevicesCommand =>
+            _scanDevicesCommand ?? (_scanDevicesCommand = new DelegateCommand(ExecuteScanDevicesCommand));
 
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             Title = "Стартовая страница";
             NavigationService = navigationService;
-            FillBondedDevices();
+            AvailableDevices = new ObservableCollection<BluetoothDevice>();
+            Device.StartTimer(TimeSpan.FromMilliseconds(200), TimerTickCallBack);
+          FillBondedDevices();
+        }
+
+        private bool TimerTickCallBack()
+        {
+            if (MainActivityModel.BluetoothDevices.Count > 0)
+            {
+                foreach (BluetoothDevice foundedDevice in MainActivityModel.BluetoothDevices)
+                {
+                    int coincidenceCount = AvailableDevices.Where(p => p.Name == foundedDevice.Name).Count();
+                    if (AvailableDevices.Count == 0 || coincidenceCount == 0)
+                    {
+                        AvailableDevices.Add(foundedDevice);
+                    }
+                }
+
+                //if (SelectedDevice != null && SelectedDevice.BondState == Bond.Bonded)
+                //{
+
+                //    string sendingParameters = "0,20,30,40,50,60" + '\n';
+                //    char[] byteBuffer = sendingParameters.ToCharArray();
+                //    Encoding utf8 = Encoding.UTF8;
+                //    byte[] buffer = utf8.GetBytes(byteBuffer);
+                //    socket.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+                //}
+            }
+            return true;
         }
 
         private void FillBondedDevices()
         {
             IBluetoothAdapter bluetoothAdapter = DependencyService.Resolve<IBluetoothAdapter>();
-            Available_Bounded_Devices = bluetoothAdapter.BondedDevices;
+            AvailableBondedDevices = bluetoothAdapter.BondedDevices;
         }
 
         private void ExecuteDeviceSelectedCommand()
         {
-            if (SelectedBoundedDevice != null)
+            if (SelectedBondedDevice != null)
             {
                 NavigationParameters parameter = new NavigationParameters();
-                parameter.Add("SelectedDevice", SelectedBoundedDevice);
-                SelectedBoundedDevice = null;
+                parameter.Add("SelectedDevice", SelectedBondedDevice);
+                SelectedBondedDevice = null;
                 NavigationService.NavigateAsync("ChangeModes", parameter);
+            }
+        }
+
+
+        void ExecuteScanDevicesCommand()
+        {
+            MainActivityModel.BluetoothDevices.Clear();
+           AvailableDevices.Clear();
+            try
+            {
+                if (MainActivityModel.BlutoothAdapter.IsDiscovering)
+                {
+                    MainActivityModel.BlutoothAdapter.CancelDiscovery();
+                }
+
+                MainActivityModel.BlutoothAdapter.StartDiscovery();
+            }
+
+            // var res=mBluetoothAdapter.
+
+            catch (Exception ex)
+            {
+                //  DisplayAlert("ex", ex.ToString(), "ok");
             }
         }
     }
